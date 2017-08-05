fb1b99a1316bbfd3fd0d56f3ed92a0a44f3adff9
rkiang <ryanlkiang@gmail.com>
2017-08-05 14:34:24 -0500

    added input fields and buttons, and logged click function

--- public/client.js
+++ public/client.js
@@ -1,1 +1,7 @@
 console.log('client.js is sourced in');
+
+$(document).ready(function(){
+    $('#mathSolution').on('click', function(){
+        console.log('Solution button was clicked');
+    });
+});
